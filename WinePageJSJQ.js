f9fe4a3d238e9a116d1ac0f198dee4b839050446
chrism3027 <chris.m.mcclure@gmail.com>
2015-08-26 22:30:58 -0500

    inital commit

---
+++ WinePageJSJQ.js
@@ -0,0 +1,49 @@
+function CalculateTotal() {
+ 
+    var answers = [Q1, Q2, Q3];
+    alert(answers[0]);
+    
+}
+
+
+/*JSon Object to store quiz results 
+var quizResults = {
+    Q1: undefined,
+    Q2: undefined,
+    Q3: undefined,
+    Q4: undefined,
+    Q5: undefined,
+    Q6: undefined,
+    Q7: undefined,
+    Q8: undefined,
+    Q9: undefined,
+    Q10: undefined
+};
+
+/*form into variable to use later 
+var $form = $("#QuizBox");
+
+$(function() {
+    $form.submit(function(){
+       
+        var data = JSON.parse(JSON.stringify($(this).serializeArray()));
+        quizResults.Q1 = data[0].value;
+//        quizResults.Q2 = data[1].value;
+//        quizResults.Q3 = data[2].value;
+//        quizResults.Q4 = data.Q4;
+//        quizResults.Q5 = data.Q5;
+//        quizResults.Q6 = data.Q6;
+//        quizResults.Q7 = data.Q7;
+//        quizResults.Q8 = data.Q8;
+//        quizResults.Q9 = data.Q9;
+//        quizResults.Q10 = data.Q10;
+        alert(quizResults.Q1);
+        return false;
+    });
+});
+*/
+
+
+/*
+<!--Nav Bar-->
+*/
